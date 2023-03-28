1408f2dbcb432bf722b8799fb3ed76ed865d4dbe
Dmitrii Marov <marov@progressivemedia.ru>
2023-03-28 22:00:22 +0300

    тест для extraReducer  user

--- src/services/user/__tests__/user-slice.test.ts
+++ src/services/user/__tests__/user-slice.test.ts
@@ -1,3 +1,4 @@
+import { fetchUserProfile, userLogin, userLogout, userRegister } from '../actions/action-creators';
 import UserReducer, { UserState, resetError, setCredentials, setError, setLoading } from '../reducers/user-slice';
 
 describe('order-data-slice', () => {
@@ -12,11 +13,12 @@ describe('order-data-slice', () => {
     error: '',
   }
 
+  const user = {
+    email: 'test@test.tt',
+    name: 'test',
+  }
+
   it('should be set credentials', () => {
-    const user = {
-      email: 'test@test.tt',
-      name: 'test',
-    }
     const action = { type: setCredentials.type, payload: user }
     const result = UserReducer(state, action);
 
@@ -45,6 +47,105 @@ describe('order-data-slice', () => {
 
     expect(result.hasError).toBeFalsy();
     expect(result.error).toEqual('');
+  })
+
+  it('should be "userLogin.pending" action', () => {
+    const result = UserReducer(state, userLogin.pending)
+
+    expect(result.isLoading).toBeTruthy();
+  })
+
+  it('should be "userLogin.fulfilled" action', () => {
+    const action = { type: userLogin.fulfilled.type, payload: {user} }
+    const result = UserReducer(state, action)
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.isAuth).toBeTruthy();
+    expect(result.hasError).toBeFalsy();
+    expect(result.error).toBe('');
+    expect(result.user).toEqual(user);
+  })
+
+  it('should be "userLogin.rejected" action', () => {
+    const result = UserReducer(state, {type: userLogin.rejected.type, payload: 'error'})
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.hasError).toBeTruthy();
+    expect(result.error).toBe('error');
+  })
+
+  it('should be "userRegister.pending" action', () => {
+    const result = UserReducer(state, userRegister.pending)
+
+    expect(result.isLoading).toBeTruthy();
+  })
+
+  it('should be "userRegister.fulfilled" action', () => {
+    const action = { type: userRegister.fulfilled.type, payload: {user} }
+    const result = UserReducer(state, action)
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.isAuth).toBeTruthy();
+    expect(result.hasError).toBeFalsy();
+    expect(result.error).toBe('');
+    expect(result.user).toEqual(user);
+  })
+
+  it('should be "userRegister.rejected" action', () => {
+    const result = UserReducer(state, {type: userRegister.rejected.type, payload: 'error'})
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.hasError).toBeTruthy();
+    expect(result.error).toBe('error');
+  })
+
+  it('should be "fetchUserProfile.pending" action', () => {
+    const result = UserReducer(state, fetchUserProfile.pending)
+
+    expect(result.isLoading).toBeTruthy();
+  })
+
+  it('should be "fetchUserProfile.fulfilled" action', () => {
+    const action = { type: fetchUserProfile.fulfilled.type, payload: {user} }
+    const result = UserReducer(state, action)
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.hasError).toBeFalsy();
+    expect(result.error).toBe('');
+    expect(result.user).toEqual(user);
+  })
+
+  it('should be "fetchUserProfile.rejected" action', () => {
+    const result = UserReducer(state, {type: fetchUserProfile.rejected.type, payload: 'error'})
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.hasError).toBeTruthy();
+    expect(result.error).toBe('error');
+  })
+
+  it('should be "userLogout.pending" action', () => {
+    const result = UserReducer(state, userLogout.pending)
+
+    expect(result.isLoading).toBeTruthy();
+  })
+
+  it('should be "userLogout.fulfilled" action', () => {
+    const action = { type: userLogout.fulfilled.type, payload: {user} }
+    const result = UserReducer(state, action)
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.isAuth).toBeFalsy();
+    expect(result.hasError).toBeFalsy();
+    expect(result.error).toBe('');
+    expect(result.user).toEqual({name: '', email: ''});
+  })
+
+  it('should be "userLogout.rejected" action', () => {
+    const result = UserReducer(state, {type: userLogout.rejected.type, payload: 'error'})
+
+    expect(result.isLoading).toBeFalsy();
+    expect(result.hasError).toBeTruthy();
+    expect(result.error).toBe('error');
   })
 })
 
